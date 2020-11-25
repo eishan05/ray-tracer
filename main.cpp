b233dad8ac44bf3e5ef793bcd2b0bcd04efcd66f
Eishan Lawrence <eishanlawrence5@gmail.com>
2020-11-24 23:46:23 -0800

    Cleanup part 1

--- main.cpp
+++ main.cpp
@@ -54,26 +54,8 @@ vec3 calculateColor(const ray& r, hitable* world, int depth) {
 
 int main() {
     (void)!freopen("output.ppm", "w", stdout);
-    // int nx = 600;
-    // int ny = 300;
-    // int ns = 100;
-    // vec3 lookfrom(3, 3, 2);
-    // vec3 lookat(0, 0, -1);
-    // float dist_to_focus = (lookfrom - lookat).length();
-    // float aperture = 2.0;
-    // camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus);
-    // float R = cos(M_PI/4);
-    // // hitable* list[4];
-    // // list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
-    // // list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
-    // // list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-    // // list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    // // list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.1));
-    // // list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-    // // list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-    // hitable *world = random_scene();
-
-       // Image
+    
+    // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
     const int nx = 1200;
