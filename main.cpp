7fa7a83f15096ae6098dda553539676b6031f1d6
Eishan Lawrence <eishanlawrence5@gmail.com>
2020-11-24 23:42:21 -0800

    Finished part 1!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,36 @@
 #include "float.h"
 #include "camera.h"
 
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n + 1];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; ++a) {
+        for (int b = -11; b < 11; ++b) {
+            float choose_mat = drand48();
+            vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * drand48()));
+                } else {
+                    // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list, i);
+}
+
 vec3 calculateColor(const ray& r, hitable* world, int depth) {
     hit_record rec;
     if (world->hit(r, 0.001, MAXFLOAT, rec)) {
@@ -24,20 +54,48 @@ vec3 calculateColor(const ray& r, hitable* world, int depth) {
 
 int main() {
     (void)!freopen("output.ppm", "w", stdout);
-    int nx = 600;
-    int ny = 300;
-    int ns = 100;
-    camera cam(90, float(nx)/float(ny));
-    float R = cos(M_PI/4);
-    hitable* list[2];
-    list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
-    list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
-    // list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-    // list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    // list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.1));
-    // list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-    // list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-    hitable *world = new hitable_list(list, 2);
+    // int nx = 600;
+    // int ny = 300;
+    // int ns = 100;
+    // vec3 lookfrom(3, 3, 2);
+    // vec3 lookat(0, 0, -1);
+    // float dist_to_focus = (lookfrom - lookat).length();
+    // float aperture = 2.0;
+    // camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+    // float R = cos(M_PI/4);
+    // // hitable* list[4];
+    // // list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
+    // // list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
+    // // list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    // // list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    // // list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.1));
+    // // list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+    // // list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
+    // hitable *world = random_scene();
+
+       // Image
+
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int nx = 1200;
+    const int ny = static_cast<int>(nx / aspect_ratio);
+    const int ns = 10;
+    const int max_depth = 50;
+
+    // World
+
+    auto world = random_scene();
+
+    // Camera
+
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+    // Render
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
