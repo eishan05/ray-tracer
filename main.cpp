789fbfb50eb654ab0c7f9c72cfd08790944bfebb
Eishan Lawrence <eishanlawrence5@gmail.com>
2020-11-24 20:43:10 -0800

    Added anti-aliasing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 
 float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
@@ -17,7 +18,7 @@ float hit_sphere(const vec3& center, float radius, const ray& r) {
     }
 }
 
-vec3 backGroundColor(const ray& r, hitable* world) {
+vec3 calculateColor(const ray& r, hitable* world) {
     hit_record rec;
     if (world->hit(r, 0.0, MAXFLOAT, rec)) {
         return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
@@ -32,10 +33,8 @@ int main() {
      (void)!freopen("output.ppm", "w", stdout);
     int nx = 600;
     int ny = 300;
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
+    int ns = 100;
+    camera cam;
     hitable* list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
@@ -43,10 +42,15 @@ int main() {
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = backGroundColor(r, world);
+            vec3 col(0.0, 0.0, 0.0);
+            for (int s = 0; s < ns; ++s) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += calculateColor(r, world);
+            }
+            col /= float(ns);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
